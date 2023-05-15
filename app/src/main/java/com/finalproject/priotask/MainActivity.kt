e56875d2520c0c167506d55810b4282940e33b6a
LAPTOP-0M7HV6AE\algier <algier@firstpayment.co.id>
2023-05-15 16:53:47 +0700

    updated kotlin plugin and compose compiler version

--- app/src/main/java/com/finalproject/priotask/MainActivity.kt
+++ app/src/main/java/com/finalproject/priotask/MainActivity.kt
@@ -10,8 +10,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
