1c04cf0e6847a7dc1f6550d7999386eec7f1622d
LAPTOP-0M7HV6AE\algier <algier@firstpayment.co.id>
2023-05-25 16:58:45 +0700

    home screen ui list content and button

--- app/src/main/java/com/finalproject/priotask/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/finalproject/priotask/presentation/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.finalproject.priotask.presentation.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -9,46 +8,57 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.finalproject.priotask.R
 import com.finalproject.priotask.ui.theme.PrioTaskTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreen(
     uiState: HomeUiState = HomeUiState()
 ) {
+    val refreshing = remember { false }
+    val pullRefreshState = rememberPullRefreshState(refreshing, {  })
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 18.dp, vertical = 24.dp)
+            .pullRefresh(pullRefreshState)
     ) {
-        LazyColumn {
+        PullRefreshIndicator(refreshing, pullRefreshState, Modifier.align(Alignment.TopCenter))
+        LazyColumn(Modifier.fillMaxSize()) {
             item {
-                HomeHeader()
+                Spacer(modifier = Modifier.height(24.dp))
+                HomeHeader(Modifier.padding(horizontal = 18.dp))
             }
             item {
                 Row {
 
                 }
             }
-            item {
-                TaskContentList()
+            items(10) {
+                TaskCard(
+                    modifier = Modifier.padding(horizontal = 18.dp)
+                )
+                Spacer(modifier = Modifier.height(24.dp))
             }
         }
         Button(
             onClick = {},
             modifier = Modifier
                 .fillMaxWidth()
-                .align(Alignment.BottomCenter),
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 24.dp, start = 18.dp, end = 18.dp),
             shape = RoundedCornerShape(12.dp)
         ) {
             Text(
@@ -61,18 +71,26 @@ fun HomeScreen(
 }
 
 @Composable
-fun TaskContentList(
-
+fun TaskCard(
+    modifier: Modifier = Modifier
 ) {
-
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(100.dp),
+        elevation = 8.dp,
+        shape = RoundedCornerShape(12.dp)
+    ) {
+        
+    }
 }
 
 @Composable
 fun HomeHeader(
-
+    modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
@@ -86,7 +104,7 @@ fun HomeHeader(
                 .weight(1f)
                 .padding(start = 16.dp, end = 16.dp)
         ) {
-            Text(text = "Hello, User", fontWeight = FontWeight.Bold)
+            Text(text = "Hello, User", fontWeight = FontWeight.Bold, fontSize = 20.sp)
             Text(text = "algirmhs@gmail.com", color = Color.Gray, fontSize = 12.sp)
         }
         IconButton(onClick = { /* Handle settings button click */ }) {
@@ -98,7 +116,7 @@ fun HomeHeader(
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
     PrioTaskTheme {
